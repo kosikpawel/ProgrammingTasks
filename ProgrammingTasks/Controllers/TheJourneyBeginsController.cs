c13f0b19659c8773983cc9c6c9434afcbe16223b
Pawel <piko333@wp.pl>
2022-09-05 20:09:34 +0200

    add centuryFromYear task

--- ProgrammingTasks/Controllers/TheJourneyBeginsController.cs
+++ ProgrammingTasks/Controllers/TheJourneyBeginsController.cs
@@ -6,10 +6,22 @@ namespace ProgrammingTasks.Controllers
     [Route("TheJourneyBegins")]
     public class TheJourneyBeginsController : ControllerBase
     {
-        [HttpGet("{param1}/{param2}")]
+        [HttpGet("add/{param1}/{param2}")]
         public ActionResult<int> Add([FromRoute] int param1, [FromRoute] int param2)
         {
             int result = param1 + param2;
+            return Ok(result);
+        }
+
+        [HttpGet("centuryFromYear/{year}")]
+        public ActionResult<int> CenturyFromYear([FromRoute] int year)
+        {
+            int century, modulo;
+            century = (year / 100);
+            modulo = (year % 100);
+
+            int result = modulo == 0 ? century : century + 1;
+
             return Ok(result);
         }
     }
