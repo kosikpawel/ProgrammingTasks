abece0df970d77aa083c9847cdc4410c16c07947
Pawel <piko333@wp.pl>
2022-09-05 20:00:14 +0200

    Add project files.

---
+++ ProgrammingTasks/Controllers/TheJourneyBeginsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ProgrammingTasks.Controllers
+{
+    [ApiController]
+    [Route("TheJourneyBegins")]
+    public class TheJourneyBeginsController : ControllerBase
+    {
+        [HttpGet("{param1}/{param2}")]
+        public ActionResult<int> Add([FromRoute] int param1, [FromRoute] int param2)
+        {
+            int result = param1 + param2;
+            return Ok(result);
+        }
+    }
+}
